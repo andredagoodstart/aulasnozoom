16061f881225204221772a990b70b81d442b9d81
andredagoodstart <119262825+andredagoodstart@users.noreply.github.com>
2023-08-23 17:12:20 -0300

    Update index.js

--- CDN/js/index.js
+++ CDN/js/index.js
@@ -27,6 +27,17 @@ function websdkready() {
 
   // some help code, remember mn, pwd, lang to cookie, and autofill.
   
+  document.getElementById("meeting_number").value = testTool.getCookie(
+    "meeting_number"
+  );
+  document.getElementById("meeting_pwd").value = testTool.getCookie(
+    "meeting_pwd"
+  );
+  if (testTool.getCookie("meeting_lang"))
+    document.getElementById("meeting_lang").value = testTool.getCookie(
+      "meeting_lang"
+    );
+  
   if (testTool.getCookie("meeting_lang"))
     document.getElementById("meeting_lang").value = testTool.getCookie(
       "meeting_lang"
